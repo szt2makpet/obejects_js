da74e76bbca981d9b6604e2247dbfa28d4d29e65
szt2makpet <szt2makpet@vasvari.org>
2023-10-13 08:40:30 +0200

    jav.

--- objects1/js.js
+++ objects1/js.js
@@ -3,8 +3,6 @@ void function functionName()
  const person = {firstName:"John", lastName:"Smith", color:"white"};
  person.firstName = "Paul"
 
-document.getElementById("demo").innerHTML =
-person.firstName + " " + person.lastName;
 console.log(person);
 
 delete person.firstName;
